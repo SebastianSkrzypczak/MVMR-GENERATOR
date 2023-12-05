e136a0aa8e46f66381f48101c13f24d580829b5c
SebastianSkrzypczak <sebastian.skrzypczak98@gmail.com>
2023-12-05 12:43:31 +0100

    venv

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\PROJEKTY\MVMR\_NEW_MVMR\MVMR-GENERATOR\venv
+set VIRTUAL_ENV=D:\Prywatne\VSC\MVMR-PYTHON\_NEW_MVMR\MVMR-GENERATOR\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
