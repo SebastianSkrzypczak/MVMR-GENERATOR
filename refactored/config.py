7d74debf7f91e55eb6622bea5b244225ae9cbec7
Sebastian Skrzypczak <sebastian.skrzypczak98@gmail.com>
2024-04-09 21:50:57 +0200

    created docker files

--- refactored/config.py
+++ refactored/config.py
@@ -2,12 +2,8 @@ import os
 
 
 def get_postgres_uri():
-    host = os.environ.get("DB_HOST", "localhost")
-    port = 5432 if host == "localhost" else 5432
-    password = os.environ.get("DB_PASSWORD", "admin")
-    user, db_name = "mvmr", "mvmr"
-    encoding = "unicode"
-    return f"postgresql://{user}:{password}@{host}:{port}/{db_name}?client_encoding={encoding}"
+
+    return os.environ.get("DB_URL")
 
 
 def get_settings_for_random_generation():
